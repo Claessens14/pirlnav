3b945d5a458a7993a0a30b6ea53fce58bc086339
Claessens14 <jacobwclaessens@gmail.com>
2023-02-01 17:49:23 -0500

    sbatch now submits

--- scripts/1-objectnav-il-powerPC.sh
+++ scripts/1-objectnav-il-powerPC.sh
@@ -3,18 +3,16 @@
 #SBATCH --time=3:00:00
 #SBATCH --partition=compute_full_node
 #SBATCH --gpus-per-node 4
-#SBATCH --nodes 1
+#SBATCH --nodes 2
 #SBATCH --ntasks-per-node 4
-#SBATCH --signal=USR1@1000
-#SBATCH --constraint=a40
 #SBATCH --output=slurm_logs/ddpil-train-%j.out
 #SBATCH --error=slurm_logs/ddpil-train-%j.err
-#SBATCH --requeue
 
 # source /srv/flash1/rramrakhya6/miniconda3/etc/profile.d/conda.sh
 # conda deactivate
 #conda activate pirlnav
 
+module load MistEnv/2020a cuda/10.2.89 gcc/8.4.0 anaconda3/2019.10 cudnn/7.6.5.32 pybind11/2.6.2\
 source activate pirlnav-v3
 cd $SCRATCH/pirlnav
 
@@ -36,7 +34,8 @@ config="configs/experiments/il_objectnav.yaml"
 # CHECKPOINT_DIR="data/new_checkpoints/objectnav_il/${dataset}/ovrl_resnet50/seed_1/"
 
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d/objectnav_hm3d_10k/"
-TENSORBOARD_DIR="tb/objectnav_il/overfitting/ovrl_resnet50/seed_3_wd_zero/"
+DATA_PATH="DATA_PATH=data/datasets/objectnav/objectnav_hm3d/objectnav_hm3d_hd"
+#TENSORBOARD_DIR="tb/objectnav_il/overfitting/ovrl_resnet50/seed_3_wd_zero/"
 CHECKPOINT_DIR="data/new_checkpoints/objectnav_il/overfitting/ovrl_resnet50/seed_3_wd_zero/"
 INFLECTION_COEF=3.234951275740812
 
